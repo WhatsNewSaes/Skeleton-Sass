ded6e6036ea8633c4093361e58fdc659642c97b8
WhatsNewSaes <scoelen89@gmail.com>
2014-12-11 22:10:36 -0500

    Adds initial files

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    "css/skeleton.css": "scss/skeleton.scss"
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: false,
+            },
+            styles: {
+                files: ['scss/**/*.scss'], // which files to watch
+                tasks: ['sass'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['sass', 'watch']);
+};
